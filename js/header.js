a5018cb4a309e778261fff100245cb1b56c5eeba
yunee <yunee729@naver.com>
2024-06-27 15:49:54 +0900

    update

--- js/header.js
+++ js/header.js
@@ -6,36 +6,59 @@ let ham_menu = document.querySelector('.ham_menu');
 
 
 // gnb
-gnb_li.forEach(function(nav_li){
-    nav_li.addEventListener('mouseover', function(){
-        // 1100 이상일 때
-        if(window.innerWidth > 1100) {
-            header.classList.add('nav_on');
-            this.querySelector('.gnb_li_inner').style.height = '276px';
-        }
+// gnb_ul.forEach(function(nav_li){
+    // 1100 이상일 때
+    if(window.innerWidth > 1100) {
+        gnb_ul.addEventListener('mouseover', function(){
+            gnb_ul.classList.add('nav_on');
+            gnb_ul.querySelectorAll('.gnb_li_inner').forEach(sub => {sub.style.height = '290px';});
+        });
+        gnb_ul.addEventListener('mouseout', function(){
+            gnb_ul.classList.remove('nav_on');
+            gnb_ul.querySelectorAll('.gnb_li_inner').forEach(sub => {sub.style.height = '0';});
+        });
+    }
 
-        // 1100 이하일 때
-        else if(window.innerWidth <= 1100) {
+    //  1100 이하일 때
+    else if(window.innerWidth <= 1100) {
+        nav_li.addEventListener('mouseover', function(){
             let gnb_inner_li_count = this.querySelector('.gnb_li_inner').childElementCount;
 
-            this.querySelector('.gnb_li_inner').style.height = gnb_inner_li_count * 46 + 'px';
-        }
-    });
-});
-gnb_li.forEach(function(nav_li){
-    nav_li.addEventListener('mouseout', function(){
-        // 1100 이상일 때
-        if(window.innerWidth > 1100) {
-            header.classList.remove('nav_on');
-            this.querySelector('.gnb_li_inner').style.height = '0';
-        }
+            nav_li.querySelectorAll('.gnb_li_inner').style.height = gnb_inner_li_count * 46 + 'px';
+        });
+        nav_li.addEventListener('mouseout', function(){
+            nav_li.querySelectorAll('.gnb_li_inner').style.height = '0';
+        });
+    }
 
-        // 1100 이하일 때
-        else if(window.innerWidth <= 1100) {
-            this.querySelector('.gnb_li_inner').style.height = '0';
-        }
-    });
-});
+    // nav_li.addEventListener('mouseover', function(){
+    //     // 1100 이상일 때
+    //     if(window.innerWidth > 1100) {
+    //         header.classList.add('nav_on');
+    //         this.querySelector('.gnb_li_inner').style.height = '276px';
+    //     }
+
+    //     // 1100 이하일 때
+    //     else if(window.innerWidth <= 1100) {
+    //         let gnb_inner_li_count = this.querySelector('.gnb_li_inner').childElementCount;
+
+    //         this.querySelector('.gnb_li_inner').style.height = gnb_inner_li_count * 46 + 'px';
+    //     }
+    // });
+
+    // nav_li.addEventListener('mouseout', function(){
+    //     // 1100 이상일 때
+    //     if(window.innerWidth > 1100) {
+    //         header.classList.remove('nav_on');
+    //         this.querySelector('.gnb_li_inner').style.height = '0';
+    //     }
+
+    //     // 1100 이하일 때
+    //     else if(window.innerWidth <= 1100) {
+    //         this.querySelector('.gnb_li_inner').style.height = '0';
+    //     }
+    // });
+// });
 
 
 // ham_menu
